9228cfd0c7ae3f174b159e639339f8f0f54583df
Leo <1624455319@qq.com>
2026-07-20 02:41:36 +0000

    add developer page

--- src/StepesRoutes.tsx
+++ src/StepesRoutes.tsx
@@ -37,6 +37,7 @@ import WebsiteTranslationVSLocalization from "./stepes/page/Stepes_Website_Trans
 import ResourcesWebsiteTranslationWorkflows from "./stepes/page/Stepes_Website_Translation_Workflows_Guide_Wireframe_v2";
 import MachineTranslationPostEditing from "./stepes/page/Stepes_Machine_Translation_Post_Editing_Complete_19_Section_Wireframe_v4";
 import ResourcesHowToTranslateAWebsite from "./stepes/page/How_to_Translate_a_Website_Guide_Wireframe_Final_QA";
+import StepesDevelopers from "./stepes/page/Stepes_Developers_Wireframe_v1.3";
 
 export default function StepesRoutes() {
   return (
@@ -167,6 +168,10 @@ export default function StepesRoutes() {
         <Route
           path="/stepes/ResourcesHowToTranslateAWebsite"
           element={<ResourcesHowToTranslateAWebsite />}
+        />
+        <Route
+          path="/stepes/StepesDevelopers"
+          element={<StepesDevelopers />}
         />
       </Route>
     </Routes>
