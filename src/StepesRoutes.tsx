5014ec6e8794e28e2c8a019099588cee496bafb2
Leo <1624455319@qq.com>
2026-07-22 04:00:21 +0000

    add1

--- src/StepesRoutes.tsx
+++ src/StepesRoutes.tsx
@@ -1,229 +1,60 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import StepesLayout from "./stepes/Layout/StepesLayout"; // 导入 Layout
 
-import StepesHomepage from "./stepes/page/stepes_homepage_wireframe_canvas";
-import StepesTechnicalHero from "./stepes/page/stepes_technical_translation_services_hero_wireframe";
-import EnterpriseSupportSections from "./stepes/page/enterprise_support_sections_wireframe";
-import StepesDemoRequest from "./stepes/page/stepes_demo_request_wireframe.tsx";
-import GlobalContentOperationsHero from "./stepes/page/global_content_operations_hero_wireframe";
-import IsoCertifiedTranslationServices from "./stepes/page/iso_certified_translation_services_wireframe";
-import ProductEngineeringLocalization from "./stepes/page/StepesProductEngineeringWireframe_Final";
-import LocalizationResourceCenter from "./stepes/page/Stepes_Resource_Center_Wireframe_Cost_Icon_Polished";
-import ProcurementTeams from "./stepes/page/Stepes_Procurement_Teams_Wireframe_Internal_Links_Final";
-import AIPoweredTranslationWorkflows from "./stepes/page/Stepes_AIPoweredTranslationWorkflows_Sections_1-12_Internal_Links_Final";
-import StepesLegalTeams from "./stepes/page/Stepes_Legal_Teams_Wireframe_Internal_Links_Pass_v8";
-import StepesAITranslationInsights from "./stepes/page/AITranslationInsightsWireframe_LinksReviewed";
-import ResourcesTranslationQuality from "./stepes/page/Stepes_Translation_Quality_Governance_Hub_Final_Reviewed";
-import ResourcesLocalizationGuides from "./stepes/page/Stepes_Localization_Guides_Wireframe_Final_Internal_Links";
-import ResourcesTranslationGuides from "./stepes/page/Stepes_Translation_Guides_Hub_Final_Eyebrow_Pass";
-import ResourcesGlobalContentStrategy from "./stepes/page/Stepes_Global_Content_Strategy_Hub_Final_Internal_Links_Pass";
-import ResourcesTranslationGlossary from "./stepes/page/Stepes_Translation_Glossary_Editorial_Directory";
-import StepesAllResourcesGuides from "./stepes/page/Stepes_All_Guides_Hub_Final_Eyebrow_Style_Fixed";
-import ResourcesFileFormatSupport from "./stepes/page/Stepes_File_Format_Support_Final_Handoff";
-import ResourcesSecurityCompliance from "./stepes/page/Stepes_Security_Compliance_Hub_Final_Refined_v3";
-import ResourcesResearchReports from "./stepes/page/Stepes_Research_Reports_Wireframe_Final_Polished";
-import ResourcesWebinars from "./stepes/page/Stepes_Webinars_Hub_Wireframe_Final_Polished";
-import StepesNewsPress from "./stepes/page/Stepes_News_Press_Hub_Wireframe_Customer_Facing_Refined";
-import APIDocumentation from "./stepes/page/Stepes_API_Documentation_Hub_Final_Polish_v13";
-import TranslationWorkflowAutomation from "./stepes/page/Stepes_Translation_Workflow_Automation_Wireframe_Final_Polished";
-import TranslationQualityAssurance from "./stepes/page/Stepes_Translation_Quality_Assurance_Wireframe_Polished_Line_Fix";
-import WebsiteTranslationServices from "./stepes/page/Stepes_Website_Translation_Services_Wireframe_Complete_Readability_Pass";
-import WebsiteTranslationProxy from "./stepes/page/Stepes_Website_Translation_Proxy_Wireframe_Complete_Final_Polish";
-import MultilingualSEOGuide from "./stepes/page/Stepes_Multilingual_SEO_Guide_Wireframe_Final";
-import WebsiteLocalizationTestingChecklist from "./stepes/page/Stepes_Website_Localization_Testing_Checklist_Wireframe_v1.6";
-import ContinuousWebsiteLocalization from "./stepes/page/Stepes_Continuous_Website_Localization_Guide_Wireframe_v1.2";
-import AIWebsiteHumanReview from "./stepes/page/AI_Website_Translation_With_Human_Review_Wireframe";
-import WebsiteTranslationVSLocalization from "./stepes/page/Stepes_Website_Translation_vs_Localization_Wireframe_v4";
-import ResourcesWebsiteTranslationWorkflows from "./stepes/page/Stepes_Website_Translation_Workflows_Guide_Wireframe_v2";
-import MachineTranslationPostEditing from "./stepes/page/Stepes_Machine_Translation_Post_Editing_Complete_19_Section_Wireframe_v4";
-import ResourcesHowToTranslateAWebsite from "./stepes/page/How_to_Translate_a_Website_Guide_Wireframe_Final_QA";
-import StepesDevelopers from "./stepes/page/Stepes_Developers_Wireframe_v1.3";
-import DevelopersTranslationAPI from "./stepes/page/Stepes_Translation_API_Developer_Page_v15";
-import SoftwareLocalizationAPI  from "./stepes/page/Stepes_Software_Localization_API_Hub_Wireframe_v1.2";
-import TranslationAPIGettingStarted from "./stepes/page/Stepes_Translation_API_Getting_Started_Wireframe_v2.3";
-import EnterpriseTranslationIntegrations from "./stepes/page/Stepes_Integrations_Hub_Wireframe_v3";
-import TranslationReportingAnalytics from "./stepes/page/Stepes_Reporting_Analytics_Hub_Wireframe_v3";
-import TranslationApprovalWorkflows from "./stepes/page/Stepes_User_Roles_Approvals_Hub_Wireframe_v3";
-import DevelopersWebhooks from "./stepes/page/Stepes_Webhooks_Wireframe_Polished_v3";
-import ContactUs from "./stepes/page/Stepes_Contact_Us_Wireframe_Final_Polished";
-import BecomeATranslator from "./stepes/page/Stepes_Become_a_Translator_Wireframe_Centered_Hero_Final";
-import TranslatorResources from "./stepes/page/Translator_Resources_Wireframe";
+import StepesLayout from "./stepes/Layout/StepesLayout";
+import routeConfig from "./stepes/routes";
+
+// 自动扫描 page 下所有 tsx/jsx
+const pages = {
+  ...import.meta.glob("./stepes/page/*.tsx"),
+  ...import.meta.glob("./stepes/page/*.jsx"),
+};
+
+const routes = routeConfig
+  .map(({ name, path }) => {
+    const importer =
+      pages[`./stepes/page/${name}.tsx`] ??
+      pages[`./stepes/page/${name}.jsx`];
+
+    if (!importer) {
+      console.warn(`找不到页面：${name}`);
+      return null;
+    }
+
+    return {
+      path,
+      Component: lazy(importer as any),
+    };
+  })
+  .filter(Boolean);
 
 export default function StepesRoutes() {
   return (
-    <Routes>
-      <Route element={<StepesLayout />}>
-        <Route path="/stepes/StepesHomepage" element={<StepesHomepage />} />
-        <Route path="/stepes/TechnicalHero" element={<StepesTechnicalHero />} />
-        <Route
-          path="/stepes/GlobalContentOperationsHero"
-          element={<GlobalContentOperationsHero />}
-        />
-        <Route
-          path="/stepes/EnterpriseSupportSections"
-          element={<EnterpriseSupportSections />}
-        />
-        <Route
-          path="/stepes/StepesDemoRequest"
-          element={<StepesDemoRequest />}
-        />
-        <Route
-          path="/stepes/IsoCertifiedTranslationServices"
-          element={<IsoCertifiedTranslationServices />}
-        />
-        <Route
-          path="/stepes/ProductEngineeringLocalization"
-          element={<ProductEngineeringLocalization />}
-        />
-        <Route
-          path="/stepes/LocalizationResourceCenter"
-          element={<LocalizationResourceCenter />}
-        />
-        <Route path="/stepes/ProcurementTeams" element={<ProcurementTeams />} />
-        <Route
-          path="/stepes/AIPoweredTranslationWorkflows"
-          element={<AIPoweredTranslationWorkflows />}
-        />
-        <Route path="/stepes/StepesLegalTeams" element={<StepesLegalTeams />} />
-        <Route
-          path="/stepes/StepesAITranslationInsights"
-          element={<StepesAITranslationInsights />}
-        />
-        <Route
-          path="/stepes/ResourcesTranslationQuality"
-          element={<ResourcesTranslationQuality />}
-        />
-        <Route
-          path="/stepes/ResourcesLocalizationGuides"
-          element={<ResourcesLocalizationGuides />}
-        />
-        <Route
-          path="/stepes/ResourcesTranslationGuides"
-          element={<ResourcesTranslationGuides />}
-        />
-        <Route
-          path="/stepes/ResourcesGlobalContentStrategy"
-          element={<ResourcesGlobalContentStrategy />}
-        />
-        <Route
-          path="/stepes/ResourcesTranslationGlossary"
-          element={<ResourcesTranslationGlossary />}
-        />
-        <Route
-          path="/stepes/StepesAllResourcesGuides"
-          element={<StepesAllResourcesGuides />}
-        />
-        <Route
-          path="/stepes/ResourcesFileFormatSupport"
-          element={<ResourcesFileFormatSupport />}
-        />
-        <Route
-          path="/stepes/ResourcesSecurityCompliance"
-          element={<ResourcesSecurityCompliance />}
-        />
-        <Route
-          path="/stepes/ResourcesResearchReports"
-          element={<ResourcesResearchReports />}
-        />
-        <Route
-          path="/stepes/ResourcesWebinars"
-          element={<ResourcesWebinars />}
-        />
-        <Route path="/stepes/StepesNewsPress" element={<StepesNewsPress />} />
-        <Route path="/stepes/APIDocumentation" element={<APIDocumentation />} />
-        <Route
-          path="/stepes/TranslationWorkflowAutomation"
-          element={<TranslationWorkflowAutomation />}
-        />
-        <Route
-          path="/stepes/TranslationQualityAssurance"
-          element={<TranslationQualityAssurance />}
-        />
-        <Route
-          path="/stepes/WebsiteTranslationServices"
-          element={<WebsiteTranslationServices />}
-        />
-        <Route
-          path="/stepes/WebsiteTranslationProxy"
-          element={<WebsiteTranslationProxy />}
-        />
-        <Route
-          path="/stepes/MultilingualSEOGuide"
-          element={<MultilingualSEOGuide />}
-        />
-        <Route
-          path="/stepes/WebsiteLocalizationTestingChecklist"
-          element={<WebsiteLocalizationTestingChecklist />}
-        />
-        <Route
-          path="/stepes/ContinuousWebsiteLocalization"
-          element={<ContinuousWebsiteLocalization />}
-        />
-        <Route
-          path="/stepes/AIWebsiteHumanReview"
-          element={<AIWebsiteHumanReview />}
-        />
-        <Route
-          path="/stepes/WebsiteTranslationVSLocalization"
-          element={<WebsiteTranslationVSLocalization />}
-        />
-        <Route
-          path="/stepes/ResourcesWebsiteTranslationWorkflows"
-          element={<ResourcesWebsiteTranslationWorkflows />}
-        />
-        <Route
-          path="/stepes/MachineTranslationPostEditing"
-          element={<MachineTranslationPostEditing />}
-        />
-        <Route
-          path="/stepes/ResourcesHowToTranslateAWebsite"
-          element={<ResourcesHowToTranslateAWebsite />}
-        />
-        <Route
-          path="/stepes/StepesDevelopers"
-          element={<StepesDevelopers />}
-        />
-        <Route
-          path="/stepes/DevelopersTranslationAPI"
-          element={<DevelopersTranslationAPI />}
-        />
-        <Route
-          path="/stepes/SoftwareLocalizationAPI"
-          element={<SoftwareLocalizationAPI />}
-        />
-        <Route
-          path="/stepes/TranslationAPIGettingStarted"
-          element={<TranslationAPIGettingStarted />}
-        />
-        <Route
-          path="/stepes/EnterpriseTranslationIntegrations"
-          element={<EnterpriseTranslationIntegrations />}
-        />
-        <Route
-          path="/stepes/TranslationReportingAnalytics"
-          element={<TranslationReportingAnalytics />}
-        />
-        <Route
-          path="/stepes/TranslationApprovalWorkflows"
-          element={<TranslationApprovalWorkflows />}
-        />
-        <Route
-          path="/stepes/DevelopersWebhooks"
-          element={<DevelopersWebhooks />}
-        />
-        <Route
-          path="/stepes/ContactUs"
-          element={<ContactUs />}
-        />
-        <Route
-          path="/stepes/BecomeATranslator"
-          element={<BecomeATranslator />}
-        />
-        <Route
-          path="/stepes/TranslatorResources"
-          element={<TranslatorResources />}
-        />
-      </Route>
-    </Routes>
+    <Suspense
+      fallback={
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          Loading...
+        </div>
+      }
+    >
+      <Routes>
+        <Route element={<StepesLayout />}>
+          {routes.map((route: any) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.Component />}
+            />
+          ))}
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
---
+++ src/stepes/routes.ts
@@ -0,0 +1,194 @@
+// src/stepes/routes.ts
+
+const routes = [
+    {
+      name: "stepes_homepage_wireframe_canvas",
+      path: "/stepes/StepesHomepage",
+    },
+    {
+      name: "stepes_technical_translation_services_hero_wireframe",
+      path: "/stepes/TechnicalHero",
+    },
+    {
+      name: "global_content_operations_hero_wireframe",
+      path: "/stepes/GlobalContentOperationsHero",
+    },
+    {
+      name: "enterprise_support_sections_wireframe",
+      path: "/stepes/EnterpriseSupportSections",
+    },
+    {
+      name: "stepes_demo_request_wireframe",
+      path: "/stepes/StepesDemoRequest",
+    },
+    {
+      name: "iso_certified_translation_services_wireframe",
+      path: "/stepes/IsoCertifiedTranslationServices",
+    },
+    {
+      name: "StepesProductEngineeringWireframe_Final",
+      path: "/stepes/ProductEngineeringLocalization",
+    },
+    {
+      name: "Stepes_Resource_Center_Wireframe_Cost_Icon_Polished",
+      path: "/stepes/LocalizationResourceCenter",
+    },
+    {
+      name: "Stepes_Procurement_Teams_Wireframe_Internal_Links_Final",
+      path: "/stepes/ProcurementTeams",
+    },
+    {
+      name: "Stepes_AIPoweredTranslationWorkflows_Sections_1-12_Internal_Links_Final",
+      path: "/stepes/AIPoweredTranslationWorkflows",
+    },
+    {
+      name: "Stepes_Legal_Teams_Wireframe_Internal_Links_Pass_v8",
+      path: "/stepes/StepesLegalTeams",
+    },
+    {
+      name: "AITranslationInsightsWireframe_LinksReviewed",
+      path: "/stepes/StepesAITranslationInsights",
+    },
+    {
+      name: "Stepes_Translation_Quality_Governance_Hub_Final_Reviewed",
+      path: "/stepes/ResourcesTranslationQuality",
+    },
+    {
+      name: "Stepes_Localization_Guides_Wireframe_Final_Internal_Links",
+      path: "/stepes/ResourcesLocalizationGuides",
+    },
+    {
+      name: "Stepes_Translation_Guides_Hub_Final_Eyebrow_Pass",
+      path: "/stepes/ResourcesTranslationGuides",
+    },
+    {
+      name: "Stepes_Global_Content_Strategy_Hub_Final_Internal_Links_Pass",
+      path: "/stepes/ResourcesGlobalContentStrategy",
+    },
+    {
+      name: "Stepes_Translation_Glossary_Editorial_Directory",
+      path: "/stepes/ResourcesTranslationGlossary",
+    },
+    {
+      name: "Stepes_All_Guides_Hub_Final_Eyebrow_Style_Fixed",
+      path: "/stepes/StepesAllResourcesGuides",
+    },
+    {
+      name: "Stepes_File_Format_Support_Final_Handoff",
+      path: "/stepes/ResourcesFileFormatSupport",
+    },
+    {
+      name: "Stepes_Security_Compliance_Hub_Final_Refined_v3",
+      path: "/stepes/ResourcesSecurityCompliance",
+    },
+    {
+      name: "Stepes_Research_Reports_Wireframe_Final_Polished",
+      path: "/stepes/ResourcesResearchReports",
+    },
+    {
+      name: "Stepes_Webinars_Hub_Wireframe_Final_Polished",
+      path: "/stepes/ResourcesWebinars",
+    },
+    {
+      name: "Stepes_News_Press_Hub_Wireframe_Customer_Facing_Refined",
+      path: "/stepes/StepesNewsPress",
+    },
+    {
+      name: "Stepes_API_Documentation_Hub_Final_Polish_v13",
+      path: "/stepes/APIDocumentation",
+    },
+    {
+      name: "Stepes_Translation_Workflow_Automation_Wireframe_Final_Polished",
+      path: "/stepes/TranslationWorkflowAutomation",
+    },
+    {
+      name: "Stepes_Translation_Quality_Assurance_Wireframe_Polished_Line_Fix",
+      path: "/stepes/TranslationQualityAssurance",
+    },
+    {
+      name: "Stepes_Website_Translation_Services_Wireframe_Complete_Readability_Pass",
+      path: "/stepes/WebsiteTranslationServices",
+    },
+    {
+      name: "Stepes_Website_Translation_Proxy_Wireframe_Complete_Final_Polish",
+      path: "/stepes/WebsiteTranslationProxy",
+    },
+    {
+      name: "Stepes_Multilingual_SEO_Guide_Wireframe_Final",
+      path: "/stepes/MultilingualSEOGuide",
+    },
+    {
+      name: "Stepes_Website_Localization_Testing_Checklist_Wireframe_v1.6",
+      path: "/stepes/WebsiteLocalizationTestingChecklist",
+    },
+    {
+      name: "Stepes_Continuous_Website_Localization_Guide_Wireframe_v1.2",
+      path: "/stepes/ContinuousWebsiteLocalization",
+    },
+    {
+      name: "AI_Website_Translation_With_Human_Review_Wireframe",
+      path: "/stepes/AIWebsiteHumanReview",
+    },
+    {
+      name: "Stepes_Website_Translation_vs_Localization_Wireframe_v4",
+      path: "/stepes/WebsiteTranslationVSLocalization",
+    },
+    {
+      name: "Stepes_Website_Translation_Workflows_Guide_Wireframe_v2",
+      path: "/stepes/ResourcesWebsiteTranslationWorkflows",
+    },
+    {
+      name: "Stepes_Machine_Translation_Post_Editing_Complete_19_Section_Wireframe_v4",
+      path: "/stepes/MachineTranslationPostEditing",
+    },
+    {
+      name: "How_to_Translate_a_Website_Guide_Wireframe_Final_QA",
+      path: "/stepes/ResourcesHowToTranslateAWebsite",
+    },
+    {
+      name: "Stepes_Developers_Wireframe_v1.3",
+      path: "/stepes/StepesDevelopers",
+    },
+    {
+      name: "Stepes_Translation_API_Developer_Page_v15",
+      path: "/stepes/DevelopersTranslationAPI",
+    },
+    {
+      name: "Stepes_Software_Localization_API_Hub_Wireframe_v1.2",
+      path: "/stepes/SoftwareLocalizationAPI",
+    },
+    {
+      name: "Stepes_Translation_API_Getting_Started_Wireframe_v2.3",
+      path: "/stepes/TranslationAPIGettingStarted",
+    },
+    {
+      name: "Stepes_Integrations_Hub_Wireframe_v3",
+      path: "/stepes/EnterpriseTranslationIntegrations",
+    },
+    {
+      name: "Stepes_Reporting_Analytics_Hub_Wireframe_v3",
+      path: "/stepes/TranslationReportingAnalytics",
+    },
+    {
+      name: "Stepes_User_Roles_Approvals_Hub_Wireframe_v3",
+      path: "/stepes/TranslationApprovalWorkflows",
+    },
+    {
+      name: "Stepes_Webhooks_Wireframe_Polished_v3",
+      path: "/stepes/DevelopersWebhooks",
+    },
+    {
+      name: "Stepes_Contact_Us_Wireframe_Final_Polished",
+      path: "/stepes/ContactUs",
+    },
+    {
+      name: "Stepes_Become_a_Translator_Wireframe_Centered_Hero_Final",
+      path: "/stepes/BecomeATranslator",
+    },
+    {
+      name: "Translator_Resources_Wireframe",
+      path: "/stepes/TranslatorResources",
+    },
+  ];
+  
+  export default routes;
