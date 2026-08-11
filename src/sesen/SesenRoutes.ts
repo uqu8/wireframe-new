b0204bdcd93151e68c7d79c0daad7c47e08c5566
Leo <1624455319@qq.com>
2026-08-11 02:12:01 +0000

    add pages 0811

--- src/sesen/SesenRoutes.ts
+++ src/sesen/SesenRoutes.ts
@@ -17,6 +17,18 @@ const routes = [
       name: "sesen_ai_assisted_clinical_translation_risks_wireframe",
       path: "/ai-assisted-clinical-translation-risks/",
     },
+    {
+      name: "Sesen_Contact_Us_Wireframe_v12",
+      path: "/contact-us/",
+    },
+    {
+      name: "Sesen_Industries_Life_Sciences_Sectors_Hub_Wireframe_v6",
+      path: "/industries/",
+    },
+    {
+      name: "Sesen_Pharmaceutical_Translation_Services_Wireframe_v3",
+      path: "/pharmaceutical-translation-services/",
+    },
   ];
   
   export default routes;
